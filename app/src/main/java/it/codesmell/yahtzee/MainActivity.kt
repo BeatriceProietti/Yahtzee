0596e3b38a05d84608a032fe104cb1dbf97339a5
giacomo-framework <giacomo200303@gmail.com>
2025-05-22 12:24:30 +0200

    Sgrunzo

--- app/src/main/java/it/codesmell/yahtzee/MainActivity.kt
+++ app/src/main/java/it/codesmell/yahtzee/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import it.codesmell.yahtzee.ui.theme.YahtzeeTheme
@@ -24,8 +28,22 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    MyScreenContent()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /* TODO: Handle button click */ }) {
+            Text("Click Me")
         }
     }
 }
